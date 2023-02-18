1d194de8942c10406d6b64daeb391e818e114190
Mazhar Zandsalimi <m47h4r@gmail.com>
2023-02-18 15:02:49 +0330

    feat(router): change to hash

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,16 @@
 import { h } from "preact";
 import { Route, Router } from "preact-router";
+import { createHashHistory } from "history";
 
 import Home from "../routes/home";
 
 const App = () => (
   <div id="app">
     <main>
-      <Router>
+      {/* wtf, right? */}
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
+      <Router history={createHashHistory()}>
         <Route path="/:*" component={Home} />
       </Router>
     </main>
